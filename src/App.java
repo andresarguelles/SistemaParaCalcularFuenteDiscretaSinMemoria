8bcb01379064474ce6e8e87ff02b14631620213e
andresarguelles <aarguellesgonzalez@gmail.com>
2024-06-26 21:53:52 -0600

    Se han agregado tres funciones nuevas; informacionTotal, redundancia y eficiencia

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
 import java.util.*;
 
-public class Main {
+public class App {
     public static Map<Character, Integer> registroDeSimbolos = new HashMap<>();
     public static Map<Character, Double> registroDeProbabilidadDeSimbolos = new HashMap<>();
     public static int totalSimbolos = 0;
+    public static double entropia = 0;
+    public static double entropiaMaxima = 0;
+    public static double informacionTotal = 0;
+    public static double redundancia = 0;
+    public static double eficiencia = 0;
+
 
     public static void calculaCoincidenciaDeCadaSimbolo(String mensaje) {
         for (int i = 0; i < mensaje.length(); i++) {
@@ -30,13 +36,31 @@ public class Main {
     }
 
     public static void calcularEntropia() {
-        double entropia = 0;
         for (Map.Entry<Character, Double> entry : registroDeProbabilidadDeSimbolos.entrySet()) {
             double probabilidad = entry.getValue();
-            entropia += probabilidad * (Math.log(probabilidad) / Math.log(2));
+            entropia += probabilidad * (Math.log(1/probabilidad) / Math.log(2));
         }
-        entropia *= -1;
-        System.out.println("La entropía es: " + entropia + " bits/símbolo");
+        System.out.println("La entropía es: " + String.format("%.3f", entropia) + " bits/símbolo");
+    }
+
+    public static void calcularInformacionTotal(){
+        for (Map.Entry<Character, Double> entry : registroDeProbabilidadDeSimbolos.entrySet()) {
+            double probabilidad = entry.getValue();
+            informacionTotal += Math.log(1/probabilidad) / Math.log(2);
+        }
+        System.out.println("La información total es: " + String.format("%.3f", informacionTotal) + " bits");
+    }
+
+    public static void calcularRedundancia(){
+        entropiaMaxima = Math.log(totalSimbolos) / Math.log(2);
+        redundancia = 1 - (entropia/ entropiaMaxima);
+        redundancia *=100;
+        System.out.println("La redundancia es: " + String.format("%.3f", redundancia) + "%" );
+    }
+
+    public static void calcularEficiencia(){
+        eficiencia = (entropia / entropiaMaxima) * 100;
+        System.out.println("La eficiencia es: " + String.format("%.3f", eficiencia) + "%");
     }
 
     public static void main(String[] args) {
@@ -50,6 +74,9 @@ public class Main {
             calcularProbabilidadDeCadaSimbolo();
             System.out.println("\n\nTotal de símbolos: " + totalSimbolos);
             calcularEntropia();
+            calcularInformacionTotal();
+            calcularRedundancia();
+            calcularEficiencia();
         } catch (Exception e) {
             System.out.println("No ingresaste ningún mensaje.");
         }
